9f06c1c48ed2a490dbca418bff512d670b1a16e6
Brianna <witherellbrianna@gmail.com>
2021-12-02 05:48:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+##### Part 1
+
+dtype1 = np.dtype([('direction', 'U1'), ('value', 'i')])
+sweeps = np.loadtxt('input.txt', dtype=dtype1, usecols=(0,1))
+
+horizontal = 0
+depth = 0
+
+for i,change in enumerate(sweeps):
+    if change['direction'] == 'd':
+        depth = depth + change['value']
+    if change['direction'] == 'u':
+        depth = depth - change['value']
+    if change['direction'] == 'f':
+        horizontal = horizontal + change['value']
+    
+print("Final Horizontal: ", horizontal)
+print("Final Depth: ", depth)
+finLoc = depth*horizontal
+print("Final Location: ", finLoc)
+
+
